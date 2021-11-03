9ed2d03f91d1bf6def830f6ae4b098efbeb2fe26
rashedul24 <ronyuap@gmail.com>
2021-11-04 01:40:41 +0600

    name, card style changed

--- js/app.js
+++ js/app.js
@@ -77,7 +77,7 @@ const updateTotal = () => {
   const grandTotal =
     getInputValue("price") + getInputValue("delivery-charge") +
     getInputValue("total-tax");
-  document.getElementById("total").innerText = grandTotal;
+  document.getElementById("total").innerText = grandTotal.toFixed(2);
 };
 
 loadProducts();
